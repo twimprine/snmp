843739738ce004bfa7615e6ed45aa3c0daf9fbac
Thomas Wimprine <twimprine@seriontechnology.com>
2018-05-23 14:46:52 -0500

    Updated URL

--- metadata.rb
+++ metadata.rb
@@ -11,10 +11,10 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-issues_url 'https://github.com/twimprine/snmp/issues'
+issues_url 'https://github.com/twimprine/xula_snmp/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-source_url 'https://github.com/twimprine/snmp'
+source_url 'https://github.com/twimprine/xula_snmp'
